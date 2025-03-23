0fe065cf280c9211269ec82be53040386a5cd1ef
Muthu <muthu.rk@gmail.com>
2025-03-23 10:05:28 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v6"; // 🔄 Increment this on each deploy
+const CACHE_VERSION = "v6.1"; // 🔄 Increment this on each deploy
 const CACHE_NAME = `chess-clock-${CACHE_VERSION}`;
 const FILES_TO_CACHE = [
   "/work-timer/",
